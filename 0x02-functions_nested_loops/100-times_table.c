24e670e774119a176eb1c7b8305832921e57dde8
DRSUCCESS <a4surprise@gmail.com>
2023-08-15 19:27:02 -0700

    function that prints the n times table, starting with 0

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,52 +1,46 @@
 #include "main.h"
 
+
 /**
- * print_times_table - function that prints the n time stable
- *
- * @n: parameter of the function
- *
- * Return: 0
- *
+ * print_times_table - prints the n times table, starting with 0
+ * @n: number of the times table
  */
 
 void print_times_table(int n)
 {
-	int i, j, result;
+	int i, j, k;
 
 	if (n >= 0 && n <= 15)
 	{
-		for (i = 0 ; i <= n ; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0 ; j <= n ; j++)
+			for (j = 0; j <= n; j++)
 			{
-				result = j * i;
+				k = j * i;
 				if (j == 0)
 				{
-					_putchar(result + '0');
-				}
-				else if (result < 10 && j != 0)
+					_putchar(k + '0');
+				} else if (k < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(result + '0');
-				}
-				else if (result > 10 && result < 100)
+					_putchar(k + '0');
+				} else if (k >= 10 && k < 100)
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((result / 10) + '0');
-
-				}
-				else if (result >= 100)
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				} else if (k >= 100)
 				{
 					_putchar(',');
 					_putchar(' ');
-					_putchar((result / 100) + '0');
-					_putchar(((result / 10) % 10) + '0');
-					_putchar((result % 10) + '0');
+					_putchar((k / 100) + '0');
+					_putchar(((k / 10) % 10) + '0');
+					_putchar((k % 10) + '0');
 				}
 			}
 			_putchar('\n');
