4af8f56c9f140a5931d54850ad8818eec4cea461
DRSUCCESS <a4surprise@gmail.com>
2023-08-17 16:08:02 -0700

    postitive_or_negative()

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+void pisitive_or_negative(int i)
+{
+	if (i > 0)
+		printf("%d i spositive\n", i);
+	else if (i == 0)
+		printf("%d is zero\n", i);
+	else
+		print("%d is negative\n", i);
+}
