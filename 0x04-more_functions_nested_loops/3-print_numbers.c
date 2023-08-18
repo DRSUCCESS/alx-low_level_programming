73f7427bcfa83a39ace05a0d8bad3ecfa0480e19
DRSUCCESS <a4surprise@gmail.com>
2023-08-17 19:34:35 -0700

    puchar again

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,15 +1,15 @@
 #include "main.h"
 
 /**
- * print_numbers - print 0-9
- * return: void
+ * print_numbers - print numbers from 0 to 9
+ * Return: void
  */
 
 void print_numbers(void)
 {
-	int c;
+	int i;
 
-	for (c = 0; c <= 9; c++)
-		_putchar(c + '0');
+	for (i = 0 ; i < 10 ; i++)
+		_putchar(i + '0');
 	_putchar('\n');
 }
